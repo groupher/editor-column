1c9774e71687f56c48151d64963127fca20e88b2
mydearxym <mydearxym@qq.com>
2020-01-01 13:54:15 +0800

    chore: update some icons

--- src/iconIndex.js
+++ src/iconIndex.js
@@ -1,16 +1,19 @@
+import diamond from './icon/diamond.svg'
+import extend from './icon/extend.svg'
+import folder from './icon/folder.svg'
+import puzzle from './icon/puzzle.svg'
+import global from './icon/global.svg'
 
 import alert from './icon/alert.svg'
 import bash from './icon/bash.svg'
 
 import explore from './icon/explore.svg'
-import fire from './icon/fire.svg'
 import github from './icon/github.svg'
 import magic from './icon/magic.svg'
 import panel from './icon/panel.svg'
 import safe from './icon/safe.svg'
 import share from './icon/share.svg'
 import sign from './icon/sign.svg'
-import signal from './icon/signal.svg'
 import tag from './icon/tag.svg'
 import tuning from './icon/tuning.svg'
 
@@ -22,62 +25,99 @@ import search from './icon/search.svg'
 import users from './icon/users.svg'
 import cloud from './icon/cloud.svg'
 
-export default [{
-    name: 'explore',
-    icon: explore,
-  }, {
-    name: 'bash',
-    icon: bash,
-  }, {
-    name: 'fire',
-    icon: fire,
-  }, {
-    name: 'github',
-    icon: github,
-  }, {
-    name: 'magic',
-    icon: magic,
-  }, {
-    name: 'panel',
-    icon: panel,
-  }, {
-    name: 'safe',
-    icon: safe,
-  }, {
-    name: 'share',
-    icon: share,
-  }, {
-    name: 'sign',
-    icon: sign,
-  }, {
-    name: 'signal',
-    icon: signal,
-  }, {
-    name: 'tag',
-    icon: tag,
-  }, {
-    name: 'tuning',
-    icon: tuning,
-  }, {
-    name: 'faq',
-    icon: faq,
-  }, {
+import tel from './icon/tel.svg'
+
+export default [
+  {
     name: 'flag',
     icon: flag,
-  }, {
+  },
+  {
+    name: 'tel',
+    icon: tel,
+  },
+  {
     name: 'tool',
     icon: tool,
-  }, {
+  },
+  {
     name: 'card',
     icon: card,
-  }, {
+  },
+  {
     name: 'search',
     icon: search,
-  }, {
+  },
+  {
     name: 'users',
     icon: users,
-  }, {
+  },
+  {
     name: 'cloud',
     icon: cloud,
+  },
+  {
+    name: 'diamond',
+    icon: diamond,
+  },
+  {
+    name: 'global',
+    icon: global,
+  },
+  {
+    name: 'extend',
+    icon: extend,
+  },
+  {
+    name: 'folder',
+    icon: folder,
+  },
+  {
+    name: 'puzzle',
+    icon: puzzle,
+  },
+  {
+    name: 'explore',
+    icon: explore,
+  },
+  {
+    name: 'bash',
+    icon: bash,
+  },
+  {
+    name: 'github',
+    icon: github,
+  },
+  {
+    name: 'magic',
+    icon: magic,
+  },
+  {
+    name: 'panel',
+    icon: panel,
+  },
+  {
+    name: 'safe',
+    icon: safe,
+  },
+  {
+    name: 'share',
+    icon: share,
+  },
+  {
+    name: 'sign',
+    icon: sign,
+  },
+  {
+    name: 'tag',
+    icon: tag,
+  },
+  {
+    name: 'tuning',
+    icon: tuning,
+  },
+  {
+    name: 'faq',
+    icon: faq,
   },
 ]
